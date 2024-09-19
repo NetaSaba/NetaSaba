51eb90726a1b8a18e71be5c1f786cd63dc5076b2
hollen9 <hollen.tw@gmail.com>
2024-09-19 22:53:27 +0000

    Initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,39 @@
+AUTHOR = 'NetaSaba民'
+SITENAME = 'NetaSaba'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Japan'
+
+DEFAULT_LANG = 'ja'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+
+OUTPUT_PATH = 'docs/'
+MARKUP = ('md', 'ipynb')  # Include 'md' if using Markdown
+
+
+# Blogroll
+LINKS = (
+    ("まめいろ鯖", "https://w.atwiki.jp/umesaba/"),
+    ("KuromeKuro", "https://sites.google.com/view/kuromekuro/welcome"),
+    ("HokkeJyuるり", "https://netasaba.github.io/hokkejyu"),
+)
+
+# Social widget
+# SOCIAL = (
+#     ("You can add links in your config file", "#"),
+#     ("Another social link", "#"),
+# )
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
